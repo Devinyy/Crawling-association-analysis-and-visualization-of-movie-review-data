9dcbcf8c6b4d2f5c62605814a6d77e5f2ab17890
Devin <1010732441@qq.com>
2020-04-20 20:44:13 +0800

    所有界面按钮跳转完成

--- 数据关联与可视化软件界面完善/WindowsClass/MainWindow.py
+++ 数据关联与可视化软件界面完善/WindowsClass/MainWindow.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import QMainWindow
 from Windows import Main_Window
 import requests
 import configparser    # 存储用户信息表
-import json
 
 
 # 创建登陆主界面
@@ -35,4 +34,37 @@ class MyMainWindow(QMainWindow, Main_Window.Ui_MainWindow ):
         # 保持登陆的session
         self.s = requests.session()# 获取登录结果（类型为 bytes）
         self.s.post(url=url_basic, headers=ua_headers, data=data)
-        
+
+        # 左侧菜单与中间副菜单绑定
+        self.listWidget.itemClicked.connect(lambda:self.switch_stack())
+        # 中间副菜单栏按钮与右侧显示栏关联
+        self.get_imform_btn.clicked.connect(lambda: self.switch_stack2(num = '0'))
+        self.check_imform_btn.clicked.connect(lambda: self.switch_stack2(num = '1'))
+        self.visual_btn.clicked.connect(lambda: self.switch_stack2(num = '2'))
+        self.visual_export_btn.clicked.connect(lambda: self.export_visual())
+
+        # 初始化所有的首页
+        self.listWidget.setCurrentRow(0)
+        self.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget_2.setCurrentIndex(0)
+
+    # 左侧菜单栏与中间副菜单栏关联
+    def switch_stack(self):
+        try:
+            i = self.listWidget.currentRow()
+            # 不是退出item
+            if str(i) != '2':
+                self.stackedWidget.setCurrentIndex(i)
+            else :
+                self.close()
+        except:
+            pass
+
+    # 中间副菜单栏与右侧显示栏关联
+    def switch_stack2(self , num ):
+        i = int (num)
+        self.stackedWidget_2.setCurrentIndex(i)
+
+    # 导出函数
+    def export_visual(self):
+        pass
